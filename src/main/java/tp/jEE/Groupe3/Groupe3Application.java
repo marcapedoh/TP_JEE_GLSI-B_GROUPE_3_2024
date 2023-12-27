8e43b773246a1ddc2c232a1ec517d5110a3eeb24
marcapedoh <adralabel@gmail.com>
2023-12-27 22:33:15 +0100

    initialisation of project

---
+++ src/main/java/tp/jEE/Groupe3/Groupe3Application.java
@@ -0,0 +1,13 @@
+package tp.jEE.Groupe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupe3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tp/jEE/Groupe3/Groupe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.jEE.Groupe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
