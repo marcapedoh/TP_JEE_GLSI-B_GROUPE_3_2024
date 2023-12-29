e9fe3c577e1ba03104189b7adeeb76d21b00110d
marcapedoh <adralabel@gmail.com>
2023-12-29 17:42:20 +0100

    fixed application  auto-shutdownError

--- src/main/java/tp/jEE/Groupe3/Groupe3Application.java
+++ src/main/java/tp/jEE/Groupe3/Groupe3Application.java
@@ -2,12 +2,12 @@ package tp.jEE.Groupe3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Groupe3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Groupe3Application.class, args);
 	}
-
 }
